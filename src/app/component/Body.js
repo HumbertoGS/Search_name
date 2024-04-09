764bff2cee2e334155c0eaeab4a443a121bd51c5
Humberto Guiracocha <humbrt75@outlook.es>
2024-04-09 16:45:47 -0500

    se agrega body para unir componentes

---
+++ src/app/component/Body.js
@@ -0,0 +1,13 @@
+import Result from "./Result/Result";
+import Search from "./Search/Search";
+
+const Body = () => {
+  return (
+    <>
+      <Search />
+      <Result />
+    </>
+  );
+};
+
+export default Body;
