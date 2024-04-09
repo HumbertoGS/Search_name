0db15688f2754c43df7ef8d933fbf69396b6ea91
Humberto Guiracocha <humbrt75@outlook.es>
2024-04-09 16:43:41 -0500

    se presenta resultados

---
+++ src/app/component/Result/Result.js
@@ -0,0 +1,29 @@
+import useFetch from "@/app/hooks/useFetch";
+
+const Result = () => {
+  const data = useFetch('1');
+
+  return (
+    <>
+      <p className="text-start px-9 pb-5 underline">Result</p>
+      <div className="flex justify-start flex-col items-center w-2/4">
+        <div className="flex flex-col w-full px-28">
+          {data &&
+            Object.entries(data).map(([key, value]) => {
+              if (key !== "id") {
+                return (
+                  <div className="flex justify-between" key={key}>
+                    <label>{key}</label>
+                    <label>{value}</label>
+                  </div>
+                );
+              }
+              return null;
+            })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Result;
