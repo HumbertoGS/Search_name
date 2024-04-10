7b40bb4796917102509bf464d024270e7a1ea573
Humberto Guiracocha <humbrt75@outlook.es>
2024-04-10 11:24:19 -0500

    se crea hooks para get y post

---
+++ src/app/hooks/useGetFetch.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const useGetFetch = (params = null) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    let url_API = `${process.env.NEXT_PUBLIC_API_LARAVEL}`;
+
+    if (params) {
+      url_API = `${url_API}/${params}`;
+
+      fetch(url_API)
+        .then((response) => response.json())
+        .then((responseData) => {
+          setData(responseData.students);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, [params]);
+
+  return data;
+};
+
+export default useGetFetch;
---
+++ src/app/hooks/usePostFetch.js
@@ -0,0 +1,37 @@
+// usePostFetch.js
+import { useEffect, useState } from "react";
+
+const usePostFetch = (dta = null, path = null) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    let url_API = `${process.env.NEXT_PUBLIC_API_LARAVEL}`;
+
+    if (path) {
+      url_API = `${url_API}/${path}`;
+    }
+
+    if (dta) {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dta),
+      };
+
+      fetch(url_API, options)
+        .then((response) => response.json())
+        .then((responseData) => {
+          setData(responseData);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, [dta]);
+
+  return data;
+};
+
+export default usePostFetch;
