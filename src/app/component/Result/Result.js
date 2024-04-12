cade04c7f462296037339f74bfc29948116de674
Humberto Guiracocha <humbrt75@outlook.es>
2024-04-12 11:31:46 -0500

    Se utiliza compoenntes creados para casos de consultas

--- src/app/component/Result/Result.js
+++ src/app/component/Result/Result.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 import useGetFetch from "@/app/hooks/useGetFetch";
 import usePostFetch from "@/app/hooks/usePostFetch";
 
+import Card from "./component/Card";
+import Items from "./component/Item";
+
 import { MdOutgoingMail } from "react-icons/md";
 
 const Result = ({ value }) => {
@@ -17,32 +20,30 @@ const Result = ({ value }) => {
 
   return (
     <>
-      {getFetchData && (
+      {getFetchData && getFetchData?.status === 200 && (
         <>
-          <p className="text-start px-9 pb-5 underline">Result</p>
-          <div className="flex justify-start flex-col items-center w-2/4">
-            <div className="flex flex-col w-full px-28">
-              {Object.entries(getFetchData).map(([key, value]) => {
-                if (key !== "id") {
-                  return (
-                    <div className="flex justify-between" key={key}>
-                      <label>{key}</label>
-                      <label>{value}</label>
-                    </div>
-                  );
-                }
-                return null;
-              })}
-            </div>
-          </div>
+          <Card>
+            {Object.entries(getFetchData?.students).map(([key, value]) => {
+              if (key !== "id") {
+                return <Items keyy={key} value={value} />;
+              }
+              return null;
+            })}
+          </Card>
 
-          <button onClick={() => sendEmail(getFetchData)}>
+          <button onClick={() => sendEmail(getFetchData?.students)}>
             <MdOutgoingMail className="w-7 h-7" />
           </button>
+
+          <div>{postFetchData && <p>{postFetchData.message}</p>}</div>
         </>
       )}
 
-      <div>{postFetchData && <p>{postFetchData.message}</p>}</div>
+      {getFetchData && getFetchData?.status !== 200 && (
+        <Card>
+          <Items keyy="Message:" value={getFetchData?.message} />
+        </Card>
+      )}
     </>
   );
 };
