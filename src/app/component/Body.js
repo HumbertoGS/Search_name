2b09d9b64a49b425d247623f61727dad143135d7
Humberto Guiracocha <humbrt75@outlook.es>
2024-04-30 14:23:39 -0500

    Se consulta datos, se llama a nuevo componente

--- src/app/component/Body.js
+++ src/app/component/Body.js
@@ -1,14 +1,24 @@
 import { useState } from "react";
+
+import useGetFetch from "@/app/hooks/useGetFetch";
+
 import Result from "./Result/Result";
 import Search from "./Search/Search";
+import AllDataName from "./Result/AllDataName";
 
 const Body = () => {
   const [value, setValue] = useState("");
 
+  const getFetchAllData = useGetFetch("students");
+
   return (
     <>
       <Search consult={(x) => setValue(x)} />
-      <Result value={value} />
+
+      <div className="flex w-full">
+        <AllDataName getFetchAllData={getFetchAllData} />
+        <Result value={value} />
+      </div>
     </>
   );
 };
